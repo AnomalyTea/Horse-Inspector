1080e7b48e29c5f69461de97dfef59300da03c3b
Josh <anomalytea@gmail.com>
2019-02-08 22:31:48 -0800

    Initialize blank project

---
+++ src/com/anomalytea/HorseInspector/HorseInspector.java
@@ -0,0 +1,16 @@
+package com.anomalytea.HorseInspector;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HorseInspector extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
