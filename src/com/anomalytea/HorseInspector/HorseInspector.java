eb97146b4db24ffe0a8eae509adc6ba8c615704b
Josh <anomalytea@gmail.com>
2019-02-10 10:46:25 -0800

    Check for updates on mod load
    Uses spigotmc.org api to check latest version and compare with its own version.

--- src/com/anomalytea/HorseInspector/HorseInspector.java
+++ src/com/anomalytea/HorseInspector/HorseInspector.java
@@ -1,4 +1,10 @@
 package com.anomalytea.HorseInspector;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HorseInspector extends JavaPlugin {
@@ -6,11 +12,31 @@ public class HorseInspector extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new PlayerAttackHorseListener(), this);
+		checkForUpdate();
 	}
 	
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	public void checkForUpdate() {
+		String tag = "[" + this.getDescription().getName() + "] ";
+		try {
+			URL url = new URL("https://api.spigotmc.org/legacy/update.php?resource=64721");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String v = rd.readLine();
+			rd.close();
+			if (v.equals(this.getDescription().getVersion())) {
+				System.out.println(tag + "Version is up-to-date.");
+			} else {
+				System.out.println(tag + "Updated version available: " + v);
+			}
+		} catch (IOException e) {
+			System.out.println(tag + "An error occurred while checking for updates.");
+		}
 	}
 
 }
