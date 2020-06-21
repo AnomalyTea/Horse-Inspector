70ffc8b577f8f696bb9629da9a594052f5f3ee64
AnomalyTea <anomalytea@gmail.com>
2020-06-21 13:29:22 -0700

    Remove unnecessary import

--- src/com/anomalytea/HorseInspector/PlayerAttackHorseListener.java
+++ src/com/anomalytea/HorseInspector/PlayerAttackHorseListener.java
@@ -16,7 +16,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerAttackHorseListener implements Listener {
 
